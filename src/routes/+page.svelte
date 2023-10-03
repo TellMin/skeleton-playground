2841d3166ae331e46e2a0b2c0fcf67862a9d327e
TellMin <dhgirkcls@gmail.com>
2023-10-03 21:59:24 +0900

    Modify: css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
 	import { filter } from '@skeletonlabs/skeleton';
 	import Indigo from '$lib/assets/indigo.svelte';
 
-	let elemCarousel: HTMLDivElement;
 	const filters = [
 		'None',
 		'Apollo',
@@ -15,38 +14,41 @@
 		'Summer84',
 		'XPro'
 	];
-	function carouselLeft(): void {
-		const x =
-			elemCarousel.scrollLeft === 0
-				? elemCarousel.clientWidth * elemCarousel.childElementCount // loop
-				: elemCarousel.scrollLeft - elemCarousel.clientWidth; // step left
-		elemCarousel.scroll(x, 0);
-	}
-	function carouselRight(): void {
-		const x =
-			elemCarousel.scrollLeft === elemCarousel.scrollWidth - elemCarousel.clientWidth
-				? 0 // loop
-				: elemCarousel.scrollLeft + elemCarousel.clientWidth; // step right
-		elemCarousel.scroll(x, 0);
-	}
+
+	let currentFilter = filters[0];
 </script>
 
+<div class="border m-8 rounded-xl border-cyan-700 flex justify-center items-center h-[40vh]">
+	<div
+		class="w-[70vw] max-h-full flex justify-center items-center"
+		use:filter={`#${currentFilter}`}
+	>
+		<Indigo />
+	</div>
+	<p class="font-mono text-sm text-gray-500">
+		{currentFilter}
+	</p>
+</div>
+
 <div class="card p-4 grid grid-cols-[auto_1fr_auto] gap-4 items-center m-8 variant-ringed-surface">
-	<!-- Button: Left -->
-	<button type="button" class="btn-icon variant-filled" on:click={carouselLeft}>
-		<i class="fa-solid fa-arrow-left" />
-	</button>
 	<!-- Full Images -->
-	<div bind:this={elemCarousel} class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
+	<div class="snap-x snap-mandatory scroll-smooth flex overflow-x-auto">
 		{#each filters as skeletonFilter}
-			<div use:filter={`#${skeletonFilter}`} class="min-w-[70%] text-center">
-				<Indigo />
-				{skeletonFilter}
+			<div use:filter={`#${skeletonFilter}`} class="text-center">
+				<button
+					class="w-12vw h-full text-sm mx-2"
+					on:click={() => (currentFilter = skeletonFilter)}
+				>
+					<Indigo />
+					{skeletonFilter}
+				</button>
 			</div>
 		{/each}
 	</div>
-	<!-- Button: Right -->
-	<button type="button" class="btn-icon variant-filled" on:click={carouselRight}>
-		<i class="fa-solid fa-arrow-right" />
-	</button>
 </div>
+
+<style>
+	.w-12vw {
+		width: 12vw;
+	}
+</style>
